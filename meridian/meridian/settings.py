99a42f9983b5cd72a3d246e7dceee65f30847108
Melanie Couillard <mrc7504@rit.edu>
2026-02-28 19:35:26 -0500

    leaderboard_page

--- meridian/meridian/settings.py
+++ meridian/meridian/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'login',    # ← add this
     'stocks',   # ← add this
+    'leaderboard'
 ]
 
 MIDDLEWARE = [
--- meridian/meridian/urls.py
+++ meridian/meridian/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
     path('stocks/', include('stocks.urls')),
+    path('leaderboard/', include('leaderboard.urls'))
 ]
